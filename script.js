e481e76c2c8373e7f0deb350fa67eb98abfc4734
Julien Baouz <baouzjulien@gmail.com>
2025-11-15 22:05:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// code initial
+const spinalCase = (string) => {
+  const regex = /(?=[A-Z])|\s+|_/;
+  const words = string.split(regex).join("-").toLowerCase();
+  console.log(words);
+  return words;
+};
 
+spinalCase("This Is Spinal Tap");
+
+// correction
+const spinalCase = str =>
+  str
+    .split(/(?:\s+|_+|(?=[A-Z]))/)
+    .filter(Boolean)
+    .join("-")
+    .toLowerCase();
